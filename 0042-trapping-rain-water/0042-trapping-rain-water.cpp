cf79112c95f827e2b30ce2b026842fb0dca8dd3e
Harshal Sharma <150684514+Harshalsharma05@users.noreply.github.com>
2026-07-02 22:56:16 +0530

    Time: 0 ms (100%), Space: 26.9 MB (40.23%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        vector<int> arr = height;
+        int n = height.size();
+        int lmax = 0, rmax = 0, total = 0;
+        int l = 0, r = n - 1;
+
+        while (l < r) {
+            if (arr[l] <= arr[r]) {
+                if (lmax > arr[l]) {
+                    total += lmax - arr[l];
+                } else {
+                    lmax = arr[l];
+                }
+                l = l + 1;
+            } else {
+                if (rmax > arr[r]) {
+                    total += rmax - arr[r];
+                } else {
+                    rmax = arr[r];
+                }
+                r = r - 1;
+            }
+        }
+
+        return total;
+    }
+};
