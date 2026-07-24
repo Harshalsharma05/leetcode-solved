7da645c81ff193b423382b0a24516176bd9b9d16
Harshal Sharma <150684514+Harshalsharma05@users.noreply.github.com>
2026-07-24 18:44:35 +0530

    Time: 0 ms (100%), Space: 9.3 MB (54.58%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    // using memoization
+    int funcM(int i, int j, vector<vector<int>> &dp) {
+        
+        if(i == 0 && j == 0) return 1;
+
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = funcM(i-1, j, dp);
+        int left = funcM(i, j-1, dp);
+
+        return dp[i][j] = up + left;
+    }
+
+    // tabulation method 
+    int func(int m, int n, vector<vector<int>> &dp) {
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                if(i > 0) { // check if not first row to so can go up
+                    up = dp[i-1][j];
+                }
+
+                if(j > 0) {
+                    left = dp[i][j-1];
+                }
+
+                dp[i][j] = up + left;
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        // return funcM(m-1, n-1, dp); // using memoization
+
+        return func(m, n, dp); // tabulation
+    }
+};
