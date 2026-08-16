d57621900dd5b13b301ed959f459b562716da1c5
Harshal Sharma <150684514+Harshalsharma05@users.noreply.github.com>
2026-08-16 22:22:57 +0530

    Time: 34 ms (69.57%), Space: 67.7 MB (74.79%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,21 +1,46 @@
+// class Solution {
+// public:
+//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+//         vector<vector<int>> ans;
+
+//         for (int i = 0; i < points.size(); i++) {
+//             int dist =
+//                 (points[i][0] * points[i][0]) + (points[i][1] * points[i][1]);
+
+//             pq.push({dist, i});
+//         }
+
+//         while (k != 0) {
+//             int idx = pq.top().second;
+//             pq.pop();
+//             ans.push_back(points[idx]);
+//             k--;
+//         }
+
+//         return ans;
+//     }
+// };
+
+// using max heap
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<int,int>>v;
-        for(int i=0;i<points.size();i++)
-        {
-            v.push_back({(points[i][0]*points[i][0]+points[i][1]*points[i][1]),i});
-        }
-        sort(v.begin(),v.end());
-        vector<vector<int>>a;
-        for(int i=0;i<v.size();i++)
-        {
-            if(k!=0)
-            {
-                a.push_back(points[v[i].second]);
-                k--;
+        priority_queue<pair<int, pair<int, int>>> maxHeap;
+        for (auto& point : points) {
+            int dist = point[0] * point[0] + point[1] * point[1];
+            maxHeap.push({dist, {point[0], point[1]}});
+            if (maxHeap.size() > k) {
+                maxHeap.pop();
             }
         }
-        return a;
+
+        vector<vector<int>> res;
+        while (!maxHeap.empty()) {
+            res.push_back({maxHeap.top().second.first,
+                           maxHeap.top().second.second});
+            maxHeap.pop();
+        }
+        return res;
     }
 };
